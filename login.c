1071d3137efc7231ffac1fdca458a79122a16e3f
omen23 <david.schuster@kdemail.net>
2016-10-13 23:57:45 +0200

    small change so hash.c rewrite plugs in

--- login.c
+++ login.c
@@ -1,19 +1,10 @@
 /* C to sqlite DB interface (for logins)
  * with hashing mechanisms using gcrypt
- * written by oMeN23 in 2011
- * If you think this is useful, use it!
- * copyleft, open and free!
+ * written by oMeN23 aka David Schuster © 2011 - 2016
+ * If you think this is useful, use it! * 
  * file: login.c (main)
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sqlite3.h>
-#include <stdbool.h>
-#include <gcrypt.h>
-#include <errno.h>
-#include <time.h>
+
 #include "login.h"
 
 /* does the user exist in the db */
@@ -57,144 +48,144 @@ bool login(const char* username,	/* username */
            bool own_sql,		/* indicates if you want your own sql statement executed against the DB */
            const char* sql_statement)	/* the sql statement to be executed against the Database or NULL */
 {
-    /* check for user overflow attempts */
-    if (stringlength(username) > USERBUF - 6 || stringlength(password) > USERBUF - 6) {
-        fprintf(stderr, "Username and/or password too long! Max. %d characters.\n", USERBUF - 6);
-        fprintf(stderr, "Make a different choice, please.\n");
-        abort();
-    }
-    else if (stringlength(username) < 2 || stringlength(password) < 4) {
-        fprintf(stderr, "Username min. 3 characters and password min. 4 characters.\n"); /* TODO ADJUST THIS IS FOR THE FINAL */
-        abort();
-    }   
-
-    /* PREREQUESITES AND MEM ALLOC */
-    sqlite3*	Db_object = NULL;
-    char*		errormsg;
-    gcrypt_init(); /* initialize mem manager and stuff so lib doesnt complain */
-    
-    login_data_t container = gcry_malloc_secure(sizeof *container);
-
-    if (container == NULL) {
-        fprintf(stderr, "Could not allocate memory!\n");
-        abort();
-    }
-    container->username = gcry_malloc_secure(USERBUF);
-    container->password = gcry_malloc_secure(USERBUF);
-    container->hash = gcry_malloc_secure(USERBUF * 2);
-
-    if (!gcry_is_secure(container->hash) || !gcry_is_secure(container) || !gcry_is_secure(container->password)) {
-        fprintf(stderr, "Could not allocate in secure memory!\n");
-        abort();
-    }
-
-    strcpy(container->username, username);  /* copy userdata in secure mem */
-    strcpy(container->password, password);
+  /* check for user overflow attempts */
+  if (stringlength(username) > USERBUF - 6 || stringlength(password) > USERBUF - 6) {
+    fprintf(stderr, "Username and/or password too long! Max. %d characters.\n", USERBUF - 6);
+    fprintf(stderr, "Make a different choice, please.\n");
+    abort();
+  }
+  else if (stringlength(username) < 2 || stringlength(password) < 4) {
+    fprintf(stderr, "Username min. 3 characters and password min. 4 characters.\n"); /* TODO ADJUST THIS IS FOR THE FINAL */
+    abort();
+  }   
+  
+  /* PREREQUESITES AND MEM ALLOC */
+  sqlite3*	Db_object = NULL;
+  char*		errormsg;
+ // gcrypt_init(); /* initialize mem manager and stuff so lib doesnt complain */
+  
+  login_data_t container = gcry_malloc_secure(sizeof *container);
+  
+  if (container == NULL) {
+    fprintf(stderr, "Could not allocate memory!\n");
+    abort();
+  }
+  container->username = gcry_malloc_secure(USERBUF);
+  container->password = gcry_malloc_secure(USERBUF);
+  container->hash = gcry_malloc_secure(USERBUF * 2);
+  
+  if (!gcry_is_secure(container->hash) || !gcry_is_secure(container) || !gcry_is_secure(container->password)) {
+    fprintf(stderr, "Could not allocate in secure memory!\n");
+    abort();
+  }
+  
+  strcpy(container->username, username);  /* copy userdata in secure mem */
+  strcpy(container->password, password);
 #ifndef HASH
-    strcpy(container->hash, password); /* SO IF SOMEBODY TURNS OFF HASHING IT WILL STILL WORK */
+  strcpy(container->hash, password); /* SO IF SOMEBODY TURNS OFF HASHING IT WILL STILL WORK */
 #endif
-    memset((void*)password, 0, stringlength(password)); /* fill the parameter with zeroes - its not secure - then it is */
-
-    int err = sqlite3_open(DATABASE,	/* const char *filename - Database filename (UTF-8), defined in the header MACRO USED */
-                           &Db_object);	/* sqlite3 **ppDb - OUT: SQLite db handle */
-
-    if (err != SQLITE_OK) {
-        fprintf(stderr, "Database connection failed, something went wrong.\n");
-        abort();
-    }    
-     
-#ifdef HASH /* call of the hashing function -> hash.c */
-    hash_func(container->password, container->hash, GCRY_MD_TIGER, GCRY_MD_FLAG_SECURE);  // TODO XXX change 6 with 306
-    fprintf(stderr, "Trying to log in as \n'%s' \nwith hashed-pw \n'%s'\n", container->username, container->hash);
+  memset((void*)password, 0, stringlength(password)); /* fill the parameter with zeroes - its not secure - then it is */
+  
+  int err = sqlite3_open(DATABASE,	/* const char *filename - Database filename (UTF-8), defined in the header MACRO USED */
+                         &Db_object);	/* sqlite3 **ppDb - OUT: SQLite db handle */
+  
+  if (err != SQLITE_OK) {
+    fprintf(stderr, "Database connection failed, something went wrong.\n");
+    abort();
+  }    
+  
+#ifdef HASH /* call of the hashing function  -> hash.c .. change to GCRY_MD_TIGER1  */
+  hash_func(GCRY_MD_TIGER, container->hash, container->password, strlen(container->password));  // TODO XXX change 6 with 306 see above for enum decl
+  fprintf(stderr, "Trying to log in as \n'%s' \nwith hashed-pw \n'%s'\n", container->username, container->hash);
 #else
-    fprintf(stderr, "Trying to log in as '%s'\n", container->username);
+  fprintf(stderr, "Trying to log in as '%s'\n", container->username);
 #endif
-    /* turn off self-supplied SQL if a null ptr or empty string is supplied */
-   
-    /* SQL STUFF AND BUILDER */
-    char sql[LARGEBUF];
-    if (own_sql)  {
-        if (longstringlength(sql_statement) >= LARGEBUF) { /* FUNC MACRO USED */
-            fprintf(stderr, "SQL statement too long. Max. %d characters.\n", LARGEBUF);
-            abort();
-        }
-         if (sql_statement == NULL || strcmp(sql_statement, "") == 0) {
-	 own_sql = false;
-	 fprintf(stderr, "login(...) called with wrong args - arg3 is true and arg4 is NULL or empty!\n");
-	}
-
-        /*if (sql_statement == NULL) {
-            fprintf(stderr, "Cannot pass NULL Pointer as SQL statement!\n");
-            abort(); not possible as for convenience, this is turned off if a null ptr is supplied
-        }*/
-        if (stringlength(sql_statement) < 6) { /* FUNC MACRO USED */
-            fprintf(stderr, "Cannot pass empty or nonsensical string as SQL statement!\n");
-            abort();
-        }
-        /* SQL OK copy it into our string - dest array should be clean so string isnt garbage */
-	memset((void*)sql, 0, sizeof(sql));
-        strcpy(sql, sql_statement);
+  /* turn off self-supplied SQL if a null ptr or empty string is supplied */
+  
+  /* SQL STUFF AND BUILDER */
+  char sql[LARGEBUF];
+  if (own_sql)  {
+    if (longstringlength(sql_statement) >= LARGEBUF) { /* FUNC MACRO USED */
+      fprintf(stderr, "SQL statement too long. Max. %d characters.\n", LARGEBUF);
+      abort();
     }
-    else /* use default sql string builder mechanism (fast and convenient and safe) 1 call per login/username */
-        build_sql_string((const char*)container->username, sql);
-
-    /* DATABASE CALL */
-    err = sqlite3_exec(Db_object,		/* An open database ![IMPORTANT -> callback is called for every ROW]! */
-                       sql,			/* SQL to be evaluated */
-                       callback,		/* Callback function */
-                       /* int (*callback)(void* freeSlot, int numDbEntries, char** DBEntries, char** ColumnName) -
-                        * arg1 is a free pointer specified by the sqlite3 calling convention interface -
-                        * I put the data to compare against in here (could be unused too)
-                        * arg2 NumDbEntries retrieves the count of entries in that row,
-                        * arg3 DBEntries is an array of strings of the data in that row and
-                        * arg4 ColumnName is an array of strings representing the column. */
-                       (void*)container,	/* 1st argument to callback */
-                       &errormsg);		/* Error msg written here */
-
+    if (sql_statement == NULL || strcmp(sql_statement, "") == 0) {
+      own_sql = false;
+      fprintf(stderr, "login(...) called with wrong args - arg3 is true and arg4 is NULL or empty!\n");
+    }
     
-    if (err != SQLITE_OK) { 
+    /*if (sql_statement == NULL) {
+     *            fprintf(stderr, "Cannot pass NULL Pointer as SQL statement!\n");
+     *            abort(); not possible as for convenience, this is turned off if a null ptr is supplied
+  }*/
+    if (stringlength(sql_statement) < 6) { /* FUNC MACRO USED */
+      fprintf(stderr, "Cannot pass empty or nonsensical string as SQL statement!\n");
+      abort();
+    }
+    /* SQL OK copy it into our string - dest array should be clean so string isnt garbage */
+    memset((void*)sql, 0, sizeof sql);
+    strcpy(sql, sql_statement);
+  }
+  else /* use default sql string builder mechanism (fast and convenient and safe) 1 call per login/username */
+    build_sql_string(sql, container->username);
+  
+  /* DATABASE CALL */
+  err = sqlite3_exec(Db_object,		/* An open database ![IMPORTANT -> callback is called for every ROW]! */
+                     sql,			/* SQL to be evaluated */
+                     callback,		/* Callback function */
+                     /* int (*callback)(void* freeSlot, int numDbEntries, char** DBEntries, char** ColumnName) -
+                      * arg1 is a free pointer specified by the sqlite3 calling convention interface -
+                      * I put the data to compare against in here (could be unused too)
+                      * arg2 NumDbEntries retrieves the count of entries in that row,
+                      * arg3 DBEntries is an array of strings of the data in that row and
+                      * arg4 ColumnName is an array of strings representing the column. */
+                     (void*)container,	/* 1st argument to callback */
+                     &errormsg);		/* Error msg written here */
+  
+  
+  if (err != SQLITE_OK) { 
     //	    ^^ er == 4 ? callback returned 1 (or any nonzero value)			
     //      fprintf(stderr, "SQL notice: callback requested query abort because a match was found.\n"); old logic part          
-            fprintf(stderr, "SQL error: %s\n", errormsg ); // sqlite3_errmsg(Db_object)
-	    sqlite3_free(errormsg);
-    }
-    /* clean up the DB connection */
-    sqlite3_close(Db_object);
-
-    /* BUFFER FLUSH */
-    memset(container->hash, 0, USERBUF * 2);
-    memset(container->password, 0, USERBUF);
-    memset(container->username, 0, USERBUF);
-    /* fprintf(stderr, "VAR TEST usr1: %s %s %s %s %s\n", username, password, container->username,
-     * container->password, container->hash); // MEM TEST FUNC to see if mem was overwritten */
-
-    /* release the memory - no data left in RAM */
-    gcry_free(container->hash);
-    gcry_free(container->password);
-    gcry_free(container->username);
-    gcry_free(container);
-    container->password = NULL;
-    container->hash = NULL;
-    container->username = NULL;
-    container = NULL;
-    /* set ptrs null, username is the only variable left intact -> see above
-     * the whole login_data_t container is now overwritten - also the passed password argument (around line 104) */
-
-    /* print messages concerning status */
-    if (!isRegistered)
-        fprintf(stderr, "Username '%s' not found in DB.\n", username);
-    /* if someone is found in the DB, but not logged in he can only have supplied a wrong password */
-    if (isRegistered && !isLoggedOn)
-        fprintf(stderr, "Wrong password for user '%s'.\n", username);
-    /* msg for logon for logfile and display, watch the supplied stream args */
-    if (isLoggedOn) {
-        fprintf(stderr, "User '%s' logged on succesful!\n", username);
-        fprintf(stdout, "Welcome %s!\n", username);
-        fprintf(stdout, "Have a nice stay.\n");
-    }
-
-    /* magic global indicating our successful login (gets set in the callback) */
-    return isLoggedOn;
+    fprintf(stderr, "SQL error: %s\n", errormsg ); // sqlite3_errmsg(Db_object)
+    sqlite3_free(errormsg);
+  }
+  /* clean up the DB connection */
+  sqlite3_close(Db_object);
+  
+  /* BUFFER FLUSH */
+  memset(container->hash, 0, USERBUF * 2);
+  memset(container->password, 0, USERBUF);
+  memset(container->username, 0, USERBUF);
+  /* fprintf(stderr, "VAR TEST usr1: %s %s %s %s %s\n", username, password, container->username,
+   * container->password, container->hash); // MEM TEST FUNC to see if mem was overwritten */
+  
+  /* release the memory - no data left in RAM */
+  gcry_free(container->hash);
+  gcry_free(container->password);
+  gcry_free(container->username);
+  gcry_free(container);
+  container->password = NULL;
+  container->hash = NULL;
+  container->username = NULL;
+  container = NULL;
+  /* set ptrs null, username is the only variable left intact -> see above
+   * the whole login_data_t container is now overwritten - also the passed password argument (around line 104) */
+  
+  /* print messages concerning status */
+  if (!isRegistered)
+    fprintf(stderr, "Username '%s' not found in DB.\n", username);
+  /* if someone is found in the DB, but not logged in he can only have supplied a wrong password */
+  if (isRegistered && !isLoggedOn)
+    fprintf(stderr, "Wrong password for user '%s'.\n", username);
+  /* msg for logon for logfile and display, watch the supplied stream args */
+  if (isLoggedOn) {
+    fprintf(stderr, "User '%s' logged on succesful!\n", username);
+    fprintf(stdout, "Welcome %s!\n", username);
+    fprintf(stdout, "Have a nice stay.\n");
+  }
+  
+  /* magic global indicating our successful login (gets set in the callback) */
+  return isLoggedOn;
 }
 
 /* small function to build the matching
@@ -203,14 +194,14 @@ bool login(const char* username,	/* username */
  * arg2 is to where to write the string
  * already in auto use of the login function if third param is false (of the login func)
  */
-void build_sql_string(const char* username, char* dest)
+void build_sql_string(char* dest, const char* username)
 {
-    char sql_string[LARGEBUF] = "";
-    strcpy(sql_string, "select * from users where username = '"); /* users is the name of the SQL table */
-    stringconcat(sql_string, username); /* FUNC MACRO USED */
-    stringconcat(sql_string, "';");
-    memset((void*)dest, 0, sizeof(dest)); /* clear mem where sql string is to be written - so theres no garbage */
-    strcpy(dest, sql_string);
+  char sql_string[LARGEBUF] = "";
+  strcpy(sql_string, "select * from users where username = '"); /* users is the name of the SQL table */
+  stringconcat(sql_string, username); /* FUNC MACRO USED */
+  stringconcat(sql_string, "';");
+  memset(dest, 0, LARGEBUF-1); /* clear mem where sql string is to be written - so theres no garbage */
+  strcpy(dest, sql_string);
 }
 
 /* Example main ... this is intented to be a "library" - only a glue code
@@ -219,27 +210,27 @@ void build_sql_string(const char* username, char* dest)
 int main(int argc, char* argv[])
 {
   
-    /* get user data - change this so it suits your needs - try to use secure storage */
-    if (!argv[1] || !argv[2]) { printf("please supply two args\n"); abort(); }
-    char* user = argv[1];
-    char* pass = argv[2];
+  /* get user data - change this so it suits your needs - try to use secure storage */
+  if (!argv[1] || !argv[2]) { printf("please supply two args\n"); abort(); }
+  char* user = argv[1];
+  char* pass = argv[2];
+  
+  /* login -> returns a bool indicating success */
+  bool logged_on = login(user,	/* self expl this variable will come back unchanged */
+                         pass,	/* this variable will be garbage after the login */
+                         false,	/* own sql statement suppplied as arg4(true) or default func(false), I'd go with this setting */
+                         NULL);	/* sql string like "select * from users;" or NULL pointer for default func */
+  
+  if (logged_on)
+  {
+    /* log-in succeeded - do what you like here */
+    return 0;
+  }
+  else
+  {
+    /* log-in didnt succeed, handle it how you like ... (call main again whatever) */
+    return 1;
     
-    /* login -> returns a bool indicating success */
-    bool logged_on = login(user,	/* self expl this variable will come back unchanged */
-                           pass,	/* this variable will be garbage after the login */
-                           false,	/* own sql statement suppplied as arg4(true) or default func(false), I'd go with this setting */
-                           NULL);	/* sql string like "select * from users;" or NULL pointer for default func */
-    
-    if (logged_on)
-    {
-        /* log-in succeeded - do what you like here */
-        return 0;
-    }
-    else
-    {
-        /* log-in didnt succeed, handle it how you like ... (call main again whatever) */
-        return 1;
-	
-    }
+  }
 }
 
