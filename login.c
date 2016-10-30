25e9560d2eecc41e1cf82b8e555ab762ba68f309
omen23 <david.schuster@kdemail.net>
2016-10-30 09:35:46 +0100

    Update login.c

--- login.c
+++ login.c
@@ -79,10 +79,10 @@ bool login(const char* username,	/* username */
     exit(2);
   }
   
-  strcpy(container->username, username);  /* copy userdata in secure mem */
+  stringcopy(container->username, username);  /* copy userdata in secure mem */
 
 #ifndef HASH
-  strcpy(container->hash, password); /* SO IF SOMEBODY TURNS OFF HASHING IT WILL STILL WORK */
+  stringcopy(container->hash, password); /* SO IF SOMEBODY TURNS OFF HASHING IT WILL STILL WORK */
 #endif
 
   int err = sqlite3_open(DATABASE,	/* const char *filename - Database filename (UTF-8), defined in the header MACRO USED */
@@ -113,11 +113,7 @@ bool login(const char* username,	/* username */
       own_sql = false;
       fprintf(stderr, "login(...) called with wrong args - arg3 is true and arg4 is NULL or empty!\n");
     }
-    
-    /*if (sql_statement == NULL) {
-     *            fprintf(stderr, "Cannot pass NULL Pointer as SQL statement!\n");
-     *            abort(); not possible as for convenience, this is turned off if a null ptr is supplied
-  }*/
+        
     if (stringlength(sql_statement) < 6) { /* FUNC MACRO USED */
       fprintf(stderr, "Cannot pass empty or nonsensical string as SQL statement!\n");
       exit(5);
@@ -143,21 +139,16 @@ bool login(const char* username,	/* username */
                      &errormsg);		/* Error msg written here */
   
   
-  if (err != SQLITE_OK) { 
-    //	    ^^ er == 4 ? callback returned 1 (or any nonzero value)			
-    //      fprintf(stderr, "SQL notice: callback requested query abort because a match was found.\n"); old logic part          
-    fprintf(stderr, "SQL error: %s\n", errormsg ); // sqlite3_errmsg(Db_object)
+  if (err != SQLITE_OK) {            
+    fprintf(stderr, "SQL error: %s\n", errormsg); /* sqlite3_errmsg(Db_object) */
     sqlite3_free(errormsg);
   }
   /* clean up the DB connection */
-  sqlite3_close(Db_object);
+  sqlite3_close(Db_object);  
   
-  
-  memset(container->hash, 0, USERBUF * 2);  
+  memset(container->hash, 0, USERBUF);  
   memset(container->username, 0, USERBUF);
-  /* fprintf(stderr, "VAR TEST usr1: %s %s %s %s %s\n", username, password, container->username,
-   * container->password, container->hash); // MEM TEST FUNC to see if mem was overwritten */
-  
+   
   /* release the memory - no data left in RAM */
   gcry_free(container->hash);  
   gcry_free(container->username);
@@ -191,12 +182,11 @@ bool login(const char* username,	/* username */
  */
 void build_sql_string(char* dest, const char* username)
 {
-  char sql_string[LARGEBUF] = "";
-  strcpy(sql_string, "select * from users where username = '"); /* users is the name of the SQL table */
+  char sql_string[LARGEBUF] = "select * from users where username = '"; /* users is the name of the SQL table */  
   stringconcat(sql_string, username); /* FUNC MACRO USED */
   stringconcat(sql_string, "';");
   memset(dest, 0, LARGEBUF-1); /* clear mem where sql string is to be written - so theres no garbage */
-  strcpy(dest, sql_string);
+  stringcopy(dest, sql_string);
 }
 
 /* Example main ... this is intented to be a "library" - only a glue code
